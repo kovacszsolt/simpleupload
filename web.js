4bae5960b43a3f6eff4e1320a2a665e7558d2cb1
Kovács Zsolt <smith.zsolt@gmail.com>
2018-09-20 13:58:03 +0200

    video thumb

--- web.js
+++ web.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const path = require('path');
 const sharp = require('sharp');
+const readChunk = require('read-chunk');
+const fileType = require('file-type');
+var ffmpeg = require('fluent-ffmpeg');
 
 let hostName = '';
 
@@ -52,8 +55,7 @@ responseObject = {
     "id": 1,
     "ref_id": "5ba008dc3cf0c12b75581277",
     "path": "",
-    "thumbnail_paths": {
-    },
+    "thumbnail_paths": {},
     "preview_image_paths": {},
     "name": "",
     "type": "IMAGE",
@@ -113,13 +115,66 @@ app.post('/send/', function (req, res, next) {
             console.log(err);
             return res.end({status: 'error'});
         }
+
+        const buffer = readChunk.sync('_public/uploads/' + req.file.originalname, 0, 4100);
+
         hostName = req.protocol + '://' + req.hostname + ':' + app.get('port');
         responseObject.path = hostName + '/uploads/' + req.file.originalname;
         responseObject.name = req.file.originalname;
-        resolutions.map((resolution) => {
-            createResolution(resolution, req.file);
-            responseObject.thumbnail_paths[resolution.key] = hostName + '/uploads/' + resolution.key + '/' + req.file.originalname;
-        });
+        console.log(fileType(buffer).mime);
+        if (fileType(buffer).mime.startsWith('image')) {
+            resolutions.map((resolution) => {
+                createResolution(resolution, req.file);
+                responseObject.thumbnail_paths[resolution.key] = hostName + '/uploads/' + resolution.key + '/' + req.file.originalname;
+            });
+        } else if (fileType(buffer).mime.startsWith('video')) {
+            const previewPath = '_public/uploads/video/' + req.file.originalname;
+            if (!fs.existsSync(previewPath)) {
+                fs.mkdirSync(previewPath);
+            }
+
+
+            var proc = ffmpeg('./_public/uploads/' + req.file.originalname)
+            // setup event handlers
+                .on('filenames', function (filenames) {
+                    console.log('screenshots are ' + filenames.join(', '));
+                })
+                .on('end', function (files) {
+                    console.log('files',files);
+                    console.log(req.file.originalname + ' - end');
+                })
+                .on('start', function () {
+                    console.log(req.file.originalname + ' start');
+                })
+                .on('error', function (err) {
+                    console.log(req.file.originalname + ' an error happened: ' + err.message);
+                })
+                // take 2 screenshots at predefined timemarks and size
+                .takeScreenshots({
+                    count: 4,
+                    size: '178x100',
+                    filename: 'preview-%i.jpg',
+                }, './_public/uploads/video/' + req.file.originalname + '/');
+
+            /*
+            ffmpeg('./_public/uploads/' + req.file.originalname).screenshots({
+                // Will take screens at 20%, 40%, 60% and 80% of the video
+                count: 4,
+                folder: './_public/'
+            }).on('start', function () {
+                console.log('ffmpeg.start');
+            }).on('end', function () {
+                console.log('ffmpeg.end');
+            }).on('error', function () {
+                console.log('ffmpeg.error');
+            }).run();
+            */
+        }
+
+        /*
+
+
+        */
         res.send(responseObject);
     })
 });
